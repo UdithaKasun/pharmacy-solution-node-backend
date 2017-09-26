5e7cd59e1f351c86e2a2f33df042f7f2d35a3301
UdithaKasun <udithakasun311@gmail.com>
2017-09-26 21:26:13 +0530

    Drug API Route Completed

--- routes/drug_api.js
+++ routes/drug_api.js
@@ -4,20 +4,25 @@
 
 var express = require('express');
 var Drug = require('../models/Drug');
-
+var Logger = require('../routes/common/Logger');
 
 var DrugRoute = express.Router();
 
-// middleware that is specific to this router
+/*
+Handling Middleware Functionality
+*/
 DrugRoute.use(function timeLog (req, res, next) {
     next()
 });
 
-// Get All Drugs from Database
+/*
+Getting all Drugs from Database.
+*/
 DrugRoute.get('/', function (req, res) {
     Drug.find({}, function (err, docs) {
         if(err){
-            res.sendStatus(500);
+            Logger.error(err.message);
+            res.status(500).send({ type : "Error", error: err.message });
             res.end();
         }
         else{
@@ -26,15 +31,19 @@ DrugRoute.get('/', function (req, res) {
     });
 });
 
-//Get a Book Identified by Id
+/* 
+Get a drug by drug serial number
+*/
 DrugRoute.get('/:id', function (req, res) {
-    Book.findById(req.params.id, function (err, books) {
+    Drug.findOne({ drug_srno : req.params.id }, function (err, drug) {
         if(err){
-            res.sendStatus(500);
+            Logger.error(err.message);
+            res.status(500).send({ type : "Error", error: err.message });
+            res.end();
         }
         else{
-            if(books != null){
-                res.json(books);
+            if(drug != null){
+                res.json(drug);
             }
             else{
                 res.sendStatus(404);
@@ -44,49 +53,62 @@ DrugRoute.get('/:id', function (req, res) {
     });
 });
 
-
-// Add New Drug
+/*
+Adding a new Drug to the Database
+*/
 DrugRoute.post('/', function (req, res) {
 var drug = new Drug(req.body);
 drug.save( (err,newDrug) => {
         if(err){
-            res.sendStatus(500);
+            Logger.error(err);
+            res.status(500).send({ type : "Error", error: err.message });
+            res.end();
         }
         else{
-            res.status(201).send({ _id : newDrug._id });
+            res.status(201).send({ drug_srno : newDrug.drug_srno });
 }
 });
 });
 
-// Delete a Existing Book
-DrugRoute.delete('/:bookId', function (req, res) {
+/*
+Deleting a drug from the database by Drug Serial Number 
+*/
+DrugRoute.delete('/:drugId', function (req, res) {
 
 
-    Book.findByIdAndRemove({_id: req.params.bookId}, (err)=>{
+    Drug.remove({drug_srno: req.params.drugId}, (err)=>{
         if(err){
-            console.log(err);
-            res.sendStatus(500);
+            Logger.error(err);
+            res.status(500).send({ type : "Error", error: err.message });
+            res.end();
         }
         else{
-            res.sendStatus(204);
+            res.sendStatus(200);
 }
 })
 
 });
 
-// Update Book
+/*
+Update Drug Detail by Drug Serial Number
+*/
 DrugRoute.put('/:id', function (req, res) {
-    Book.findById(req.params.id, function (err, book) {
-        if (err)
-            res.sendStatus(500);
+    Drug.findOne({ drug_srno : req.params.id }, function (err, drug) {
+        if (err){
+            Logger.error(err);
+            res.status(500).send({ type : "Error", error: err.message });
+            res.end();
+        }
 
-        book.title = req.body.title;
-        book.author = req.body.author;
+        drug.drug_name = req.body.drug_name;
 
-        book.save(function (err, updatedBook) {
-            if (err)
-                res.sendStatus(500);
-            res.json(updatedBook);
+        drug.save(function (err, updatedDrug) {
+            if (err){
+                Logger.error(err);
+                res.status(500).send({ type : "Error", error: err.message });
+                res.end();
+            }
+            res.sendStatus(200);
         });
     });
 });
